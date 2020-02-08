7537ff0b9d3ecc3c4567e50e19c14e06d4e8d7f8
Abdiel Athayde <46298069+abdieldeathayde@users.noreply.github.com>
2020-02-08 09:34:48 -0300

    Calculadora

---
+++ CalculaRaizQuadrada.py
@@ -0,0 +1,12 @@
+import math;
+def calculaRaizQuadrada():
+
+  numero = int(input("Digite algum numero para calcular a raiz quadrada: "));
+  
+  raiz = math.sqrt(numero);
+
+  print(raiz);
+
+
+
+calculaRaizQuadrada();  
